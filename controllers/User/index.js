18581be1424249009c8e641b5e40d90ac47eaea3
gab <gabrielfontes321@gmail.com>
2022-04-13 17:42:32 -0300

    feat: implementing listUserByIdController

--- controllers/User/index.js
+++ controllers/User/index.js
@@ -1,9 +1,11 @@
 const create = require('./createUserControllers');
 const login = require('./loginController');
 const list = require('./listUserController');
+const listById = require('./listUserByIdController');
 
 module.exports = {
   create,
   list,
   login,
+  listById,
 };
