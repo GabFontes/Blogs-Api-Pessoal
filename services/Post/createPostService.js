6f5ae263b4ef931a0eee5386c84f0cf42f31d425
gab <gabrielfontes321@gmail.com>
2022-04-14 16:27:45 -0300

    feat: implementing create post controller

---
+++ services/Post/createPostService.js
@@ -0,0 +1,27 @@
+const { BlogPost, Category, User } = require('../../models');
+const { tokenValidation } = require('../../middlewares/tokenValidation');
+
+const createPostService = async (token, data) => {
+  try {
+    const decoded = tokenValidation(token);
+    const { email } = decoded;
+
+    const { dataValues: { id } } = await User.findOne({ where: { email } });
+    const { title, content, categoryIds } = data;
+
+    const categories = categoryIds.map((idd) => Category.findByPk(idd));
+    const resolved = await Promise.all(categories);
+
+    const verify = resolved.some((cat) => cat === null);
+
+    if (verify) return { message: '"categoryIds" not found', status: 400 };
+
+    const created = await BlogPost.create({ title, content, categoryIds });
+
+    return { ...created, userId: id };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+module.exports = createPostService;
---
+++ services/Post/index.js
@@ -0,0 +1,5 @@
+const create = require('./createPostService');
+
+module.exports = {
+  create,
+};
